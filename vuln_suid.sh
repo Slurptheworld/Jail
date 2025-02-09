3dcce56acea34c3ceb265239c877e3a2bf2fcb31
Slurptheworld <144984008+Slurptheworld@users.noreply.github.com>
2025-02-09 14:51:56 +0100

    Update vuln_suid.sh

--- vuln_suid.sh
+++ vuln_suid.sh
@@ -2,5 +2,6 @@
 # Ajout d'un binaire SUID root dans le chroot
 
 chmod 4755 /var/www/html/bin/bash
+chmod 4755 /var/www/html/bin/python3
 
 echo "✅ SUID activé sur /bin/bash. Exploitation : /bin/bash -p"
